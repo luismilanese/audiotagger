a7b940d5fa5dfdffb7d40f03a88c8d9bf9f0017b
Luis Milanese <luis.milanese@gmail.com>
2026-09-10 17:02:22 -0300

    Refactor file handling to implement FileRenamer class for audio file renaming and enhance RenameResult with error handling

--- src/audiotagger/models/file_handling.py
+++ src/audiotagger/models/file_handling.py
@@ -1,31 +1,39 @@
 from pathlib import Path
 
-from audiotagger.models.formats import (
-    AudioFileContext,
-    AudioFileFactory,
-    FormatsAllowed,
-)
-from audiotagger.utils import extract_tracknumber
+from audiotagger.models.extractors import MP3MetadataExtractor
+from audiotagger.models.mask import MaskEngine
+from audiotagger.models.metadata import RenameResult
 
 
-def format_guess(file_path: str) -> FormatsAllowed | None:
-    if not Path(file_path).is_file():
-        return None
+class FileRenamer:
+    @staticmethod
+    def rename(file_path: Path, mask: str, dry_run: bool = False) -> RenameResult:
+        audio_metadata = MP3MetadataExtractor.extract(file_path=file_path)
+        new_name = MaskEngine.apply(mask, audio_metadata)
+        new_path = file_path.with_name(f"{new_name}{file_path.suffix}")
 
-    for fmt in FormatsAllowed:
-        if file_path.lower().endswith(fmt.value):
-            return fmt
-    return None
+        rename_result = RenameResult(
+            original_path=file_path,
+            new_path=new_path,
+            success=True,
+            error_message=None,
+        )
 
+        if file_path == new_path:
+            return rename_result
 
-def rename(file_path: Path):
-    guessed_format = format_guess(str(file_path))
-    if not guessed_format:
-        return
+        if new_path.exists():
+            rename_result.success = False
+            rename_result.error_message = "There is a file with this name already"
+            return rename_result
 
-    file_context = AudioFileContext(guessed_format, file_path)
-    audio_file = AudioFileFactory.create_audio_file(file_context)
-    mask = f"{extract_tracknumber(audio_file.get('tracknumber')[0])} - {audio_file.get('title')[0]}.mp3"
-    destination_folder = file_path.parent
-    new_file_path = f"{destination_folder}/{mask}"
-    file_path.rename(new_file_path)
+        if dry_run:
+            return rename_result
+
+        try:
+            file_path.rename(new_path)
+        except OSError as e:
+            rename_result.success = False
+            rename_result.error_message = str(e)
+
+        return rename_result
--- src/audiotagger/models/metadata.py
+++ src/audiotagger/models/metadata.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass
@@ -8,3 +9,11 @@ class AudioMetadata:
     album: str | None = None
     track_number: str | None = None
     year: str | None = None
+
+
+@dataclass
+class RenameResult:
+    original_path: Path
+    new_path: Path
+    success: bool
+    error_message: str | None
